545a3adeee249a70a48b9deead6030052da0dc35
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-04-30 02:20:17 -0500

    loopsss

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cards) {
+    const thankYouMessages = [];
+    for (let i = 0; i < cards.length; i++) {
+        const message = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+        thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+}
+function countDown(num) {
+    let n = num
+    while (n >= 0) {
+        console.log(n);
+        n--;
+        
+    }
+}
+    
